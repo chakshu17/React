c528f084b86cb15b07b9d71f7b3ba16f036fa615
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-13 21:47:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./App.css";
 // import Greet from "./components/Greet";
-import { Greet } from "./components/Greet";
-import Welcome from "./components/welcome";
-import Hello from "./components/Hello";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-import Stylesheets from "./components/Stylesheets";
-import Inline from "./components/Inline";
+// import { Greet } from "./components/Greet";
+// import Welcome from "./components/welcome";
+// import Hello from "./components/Hello";
+// import Message from "./components/Message";
+// import Counter from "./components/Counter";
+// import FunctionClick from "./components/FunctionClick";
+// import ClassClick from "./components/ClassClick";
+// import EventBind from "./components/EventBind";
+// import ParentComponent from "./components/ParentComponent";
+// import UserGreeting from "./components/UserGreeting";
+// import NameList from "./components/NameList";
+// import Stylesheets from "./components/Stylesheets";
+// import Inline from "./components/Inline";
 import './appStyle.css';
-import styles from './appStyles.module.css'
-import Form from "./Form module/Form";
+// import styles from './appStyles.module.css'
+// import Form from "./Form module/Form";
 function App() {
 	return (
 		<div className="App">
-		<Form/>
+
 			{/*Form module
 			<Form/>
 			CSS
