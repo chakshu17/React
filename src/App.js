80d85deeb82dac20b75fc125dde4597962a38a49
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-14 22:45:17 +0530

    HOC

--- src/App.js
+++ src/App.js
@@ -1,75 +1,87 @@
 import React from "react";
 import "./App.css";
-import ErrorBoundary from "./Error handler/ErrorBoundary";
-import Hero from "./Error handler/Hero";
+import ClickCounter from "./HIgher Order Component/ClickCounter";
+import HoverCounter from "./HIgher Order Component/HoverCounter";
+
 
 function App() {
 	return (
 		<div className="App">
-			<ErrorBoundary>
-				<Hero heroName="Batman" />
-			</ErrorBoundary>
-			<ErrorBoundary>
-				<Hero heroName="Joker" />
-			</ErrorBoundary>
-			{/*
-			portals:portals
-			refs: Refs
-
-
-			Pure Components
-			folder name: purecomponent 
-			Fragments
-				<FragmentDemo/>
-				<Table/>
-
-			Lifecycle
-			<LifecyleA/>
-
-			Form module
-			<Form/>
-			CSS
-			<Stylesheets primary={true}/>
-			2. Inline css:   <Inline/> 
-			3. css module:    
-		
-			import './appStyle.css';
-			<h1 className='error'>Error</h1>
-
-
-			<h1 className={styles.success}>Success</h1>
-			import styles from './appStyles.module.css'
-
-
-			List rendering:
-				<NameList/>	
-						
-			Conditional rendering:
-					<UserGreeting/>
-
-				<ParentComponent/>
-					<EventBind/>
-			<FunctionClick />
-			<ClassClick/>
-			<Greet name="Diana" heroName="Wonder Woman" />
-			<Welcome name="Bruce" heroName="Batman"/><Message /> 
-			<Greet></Greet>
-			<Counter></Counter>
-			<Welcome></Welcome>
-			<Hello></Hello> 
-			 <Greet name="Bruce" heroName="Batman">
-				<p>This is Children components</p>
-			</Greet>
-			<Greet name="Clark" heroName="Superman">
-				<button>Action</button>
-			</Greet>
-		
-
-			<Welcome name="Bruce" heroName="Batman"/>
-			<Welcome name="Clark" heroName="Superman"/>
-			<Welcome name="Diana" heroName="Wonder Woman"/> */}
+		<ClickCounter name="Chakshu"/>
+		<HoverCounter/>
 		</div>
+	
 	);
 }
 
 export default App;
+
+			// Higher Order Component
+			//		<ClickCounter/>
+			//    <HoverCounter/>
+
+			// Error handling
+			
+			// <ErrorBoundary>
+			// 	<Hero heroName="Batman" />
+			// </ErrorBoundary>
+			// <ErrorBoundary>
+			// 	<Hero heroName="Joker" />
+			// </ErrorBoundary>
+
+
+			// portals:portals
+			// refs: Refs
+
+
+			// Pure Components
+			// folder name: purecomponent 
+			// Fragments
+			// 	<FragmentDemo/>
+			// 	<Table/>
+
+			// Lifecycle
+			// <LifecyleA/>
+
+			// Form module
+			// <Form/>
+			// CSS
+			// <Stylesheets primary={true}/>
+			// 2. Inline css:   <Inline/> 
+			// 3. css module:    
+		
+			// import './appStyle.css';
+			// <h1 className='error'>Error</h1>
+
+
+			// <h1 className={styles.success}>Success</h1>
+			// import styles from './appStyles.module.css'
+
+
+			// List rendering:
+			// 	<NameList/>	
+						
+			// Conditional rendering:
+			// 		<UserGreeting/>
+
+			// 	<ParentComponent/>
+			// 		<EventBind/>
+			// <FunctionClick />
+			// <ClassClick/>
+			// <Greet name="Diana" heroName="Wonder Woman" />
+			// <Welcome name="Bruce" heroName="Batman"/><Message /> 
+			// <Greet></Greet>
+			// <Counter></Counter>
+			// <Welcome></Welcome>
+			// <Hello></Hello> 
+			//  <Greet name="Bruce" heroName="Batman">
+			// 	<p>This is Children components</p>
+			// </Greet>
+			// <Greet name="Clark" heroName="Superman">
+			// 	<button>Action</button>
+			// </Greet>
+		
+
+			// <Welcome name="Bruce" heroName="Batman"/>
+			// <Welcome name="Clark" heroName="Superman"/>
+			// <Welcome name="Diana" heroName="Wonder Woman"/> 
